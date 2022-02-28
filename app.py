6eeb55ca405300caca77321e286c3ea34391ffc7
Tom <tomjuggler@gmail.com>
2022-02-28 15:40:25 +0200

    Cleaned up some comments and unused code
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ class OntologyDataStore:
         if len(repos) > 1:
             subgraphs = []
             for repo in repos: 
-                ids = OntologyDataStore.getRelatedIDs(self, repo, selectedIds) #todo: do we need to differentiate?
+                ids = OntologyDataStore.getRelatedIDs(self, repo, selectedIds) 
                 subgraphs.append(self.graphs[repo].subgraph(ids))
             #test combine two graphs:
             F = networkx.compose_all(subgraphs)
@@ -241,15 +241,9 @@ class OntologyDataStore:
             pass
         return (entries)
 
-    # def getLabelForID(self, repo, ID):
-    #     for classIri in self.releases[repo].get_classes():
-    #     classIri = repo.get_classes()
-    #     label = self.releases[repo].get_annotation(classIri, app.config['RDFSLABEL']) #yes
-    #     return label
-
 ontodb = OntologyDataStore()
 
-#test function from onto-text-tag: 
+#function from onto-text-tag: 
 def get_ids(ontol_list):
     # print("get_ids running here")
     checklist = []
@@ -258,15 +252,15 @@ def get_ids(ontol_list):
         for classIri in ontol.get_classes():
             # print("for classIri running")        
             label = ontol.get_annotation(classIri, RDFSLABEL)
-            # print("label: ", label) #todo: got labels but no ID, why?
+            # print("label: ", label) 
             # label = ontol.get_annotation(classId, RDFSLABEL)
             if classIri:                
                 classId = str(classIri).rsplit('/', 1)[1].replace('_', ':').strip()
-                print(classId)
+                # print(classId)
                 if classId and label:
-                    print("got classId and labels") 
+                    # print("got classId and labels") 
                     checklist.append(classId + "|"+ label)                   
-                    print(classId, ": ", label)
+                    # print(classId, ": ", label)
                     
     return checklist
 
@@ -274,53 +268,31 @@ def get_ids(ontol_list):
 @app.route('/')
 @app.route('/home')
 def home():
-    ontologiesT = ontoDict["ontologies"]
-    print("ontologiesT: ", ontologiesT)
-    labels = get_ids(ontologiesT) 
+    ontologies_for_list = ontoDict["ontologies"]
+    # print("ontologies_for_list: ", ontologies_for_list)
+    labels = get_ids(ontologies_for_list) 
     label_list = labels
-    # print("labels: ", labels)
-
     ontologies = ["BCIO", "AddictO"]
-    # idList = []
-    # for repo in ontologies:    #todo: fix this horrible repo repo repos        
-    #     ontodb.parseRelease(repo)
-    #     allIds = ontodb.getReleaseIDs(repo)
-        # print(allIds)
-        # metaData = ontodb.getMetaData(repo, allIds)
-        # print(metaData)
-
-        # for ID in allIds: 
-        #     if ID is not None and ID != "":
-        #         # label = ontodb.getLabelForID(ID, repo)
-        #         label = "test"
-        #         # idList.append(ID.strip()) #todo: need to append label here also
-        #         idList.append(ID + "|"+ label)  
-        # print("idList is: ", idList)
-    # labels = ontodb.getReleaseIDs("BCIO") #todo: support multiple repo's 
-    # # labels = get_ids(ontoDict) 
-    # label_list={'labels': labels}
-    # json.dumps(label_list)
-    # print("home label_list is: ", label_list)
-    return render_template("index.html", label_list=label_list, ontologies=ontologies) #todo: fix label_list name is incorrect on front end, change when updated in index.html
+    return render_template("index.html", label_list=label_list, ontologies=ontologies) 
 
 @app.route('/visualise', methods=['POST'])
 def visualise():
-    print("open called")
+    # print("open called")
     #build data we need for dotStr query (new one!)
     if request.method == "POST":
         idString = request.form.get("idList")
         repo = request.form.get("repo")
         idListAll = idString.split(',')
-        print("idListAll: ", idListAll)
-        idList = [str(word).rsplit('|')[0].strip() for word in idListAll]      #todo: remove labels here worked?
-        print("split idList is: ", idList)
+        # print("idListAll: ", idListAll)
+        idList = [str(word).rsplit('|')[0].strip() for word in idListAll]      
+        # print("split idList is: ", idList)
         repos = repo.split()
         for repo in repos:    #todo: fix this horrible repo repo repos        
             ontodb.parseRelease(repo) 
-        if len(idList) == 0:
-            print("got zero length idList")
+        if len(idList) < 1 or (len(idList) == 1 and idList[0] == ""):
+            # print("got zero length idList")
             allIds = ontodb.getReleaseIDs(repo)
-            print(allIds)
+            # print(allIds)
             idList = []
             for ID in allIds: 
                 if ID is not None and ID != "":
